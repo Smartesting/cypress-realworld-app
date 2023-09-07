533363cb3409a47cadfc99cfef53807f7053af39
Joan RACENET <joan.racenet@smartesting.com>
2023-09-07 12:01:44 +0200

    log commands and test data for informative purpose

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,31 +1,38 @@
 // @ts-check
 import "@cypress/code-coverage/support";
 import "./commands";
-import {isMobile} from "./utils";
-import GravityCollector from '@smartesting/gravity-data-collector/dist'
+import { isMobile } from "./utils";
+import GravityCollector from "@smartesting/gravity-data-collector/dist";
 
-beforeEach(() => {
-    // cy.intercept middleware to remove 'if-none-match' headers from all requests
-    // to prevent the server from returning cached responses of API requests
-    cy.intercept(
-        {url: "http://localhost:3001/**", middleware: true},
-        (req) => delete req.headers["if-none-match"]
-    );
+beforeEach(function () {
+  // cy.intercept middleware to remove 'if-none-match' headers from all requests
+  // to prevent the server from returning cached responses of API requests
+  cy.intercept(
+    { url: "http://localhost:3001/**", middleware: true },
+    (req) => delete req.headers["if-none-match"]
+  );
 
-    // Throttle API responses for mobile testing to simulate real world condition
-    if (isMobile()) {
-        cy.intercept({url: "http://localhost:3001/**", middleware: true}, (req) => {
-            req.on("response", (res) => {
-                // Throttle the response to 1 Mbps to simulate a mobile 3G connection
-                res.setThrottle(1000);
-            });
-        });
-    }
+  // Throttle API responses for mobile testing to simulate real world condition
+  if (isMobile()) {
+    cy.intercept({ url: "http://localhost:3001/**", middleware: true }, (req) => {
+      req.on("response", (res) => {
+        // Throttle the response to 1 Mbps to simulate a mobile 3G connection
+        res.setThrottle(1000);
+      });
+    });
+  }
+
+  // display all info about current test
+  console.log(this.currentTest);
 });
 
 Cypress.on("window:load", (win) => {
-    GravityCollector.init({
-        authKey: "b617f074-9a2d-4f17-8038-f44d8f55c78b",
-        windowInstance: win
-    });
-})
+  GravityCollector.init({
+    authKey: "b617f074-9a2d-4f17-8038-f44d8f55c78b",
+    windowInstance: win,
+  });
+});
+
+Cypress.on("command:start", (command) => {
+  console.log(command);
+});
